e4212611cbde4fc7950d7e9d07eabd755b645fb7
lbylbylby123456 <32089961+lbylbylby123456@users.noreply.github.com>
2017-10-17 18:44:31 +0800

    Create ww27.c

---
+++ ww27.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+
+int main()
+
+{
+
+	//ax+by=m
+
+	//cx+dy=n
+
+	//x=(md-bn)/(ad-bc)
+
+	//y=(mc-an)/(bc-ad)
+
+	double a,b,c,d,m,n,x,y;
+
+	scanf("%lf%lf%lf%lf%lf%lf",&a,&b,&c,&d,&m,&n);
+
+    x=(m*d-b*n)/(a*d-b*c);
+
+    y=(m*c-a*n)/(b*c-a*d);
+
+	printf("x=%lf y=%lf\n",x,y);
+
+	system("pause");
+
+	return 0; 
+
+}
