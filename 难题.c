5eabae4a796fc357bf1622bc7a451958fdff294d
lbylbylby123456 <32089961+lbylbylby123456@users.noreply.github.com>
2017-11-01 19:19:08 +0800

    Create 难题.c

---
+++ 难题.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <math.h>
+int main (){
+	int n;
+	int i;
+	int tem;
+		
+  for (i=0;i>=tem;i++)
+	{
+	  
+	  int a;int b;int c;int d;int e;int i=1;int tem=0;int n;
+	  int x;int y;int z;int w;int t;
+		a>=1&&a<10;
+		b>=1&&b<10;
+		c>=1&&c<10;
+		d>=1&&d<10;
+		e>=1&&e<10;
+		a=i/10000;
+		x=i-10000*a;
+		if(a=1) tem=tem+1;
+		else tem=tem;
+		b=x/1000;
+		y=x-1000*b;
+		if (b=1) tem=tem+1;
+		else tem=tem;
+		c=y/100;
+		z=y-100*c;
+		if (c=1) tem=tem+1;
+		else {
+		tem=tem;} 
+		d=z/10;
+		w=z-10*d;
+		if (d=1) tem=tem+1;
+		else tem=tem;
+		e=w;
+		if (e=1) tem=tem+1;
+		else tem=tem;
+
+	     
+	}printf ("%d\n",tem);
+	return 0;
+	} 
+		
