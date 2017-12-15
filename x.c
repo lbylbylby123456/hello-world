72e4cc7bc50e911c5269bbb095c489d80c4c3e69
lbylbylby123456 <32089961+lbylbylby123456@users.noreply.github.com>
2017-12-15 15:07:17 +0800

    Create x.c

---
+++ x.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+#include <stdlib.h>
+#include <string.h>
+int main(){
+	int n;
+	scanf("%d",&n);
+
+
+if(n<=0||n>=8)
+printf("Input Error");
+
+
+if(n>0&&n<8){
+int x=2,y=3,z=4,m=5,t=0;//这别弄成n 
+
+	
+	if(n==1)
+	printf("2");
+	if(n==2)
+	printf("5");
+	if(n==3)
+	printf("1");
+	if(n==4)
+	printf("%d",7);
+	if(n==5)
+	printf("%d",2);
+	if(n==6)
+	printf("%d",3);
+	}
